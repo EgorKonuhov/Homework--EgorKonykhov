03a0ed0f2b838c262d2a0c53ef1392d825eab4fc
EgorKonuhov <115095231+EgorKonuhov@users.noreply.github.com>
2022-10-23 13:47:11 +0300

    Add files via upload

---
+++ Егор Конюхов дз 3.cpp
@@ -0,0 +1,116 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	/*cout << "Home task 3.1\n";
+	int sum= 0 ;
+	int a=0 ;
+	cout << "Введите начальное значение от 1 до 500: ";
+	cin >> a;
+	for (int i = a;a <= 500; a++)
+	{
+		sum = sum + a;
+	}
+	cout  << sum << endl; 
+	return 0;*/
+	/*cout << "Home task 3.2\n";
+	int a, b;
+
+	cout << "Введите X и Y: ";
+	cin >> a >> b;
+
+	
+
+	for (int i = 1; i < b; i++) {
+		a = a * a;
+		cout << a;
+	}*/
+	/*cout << "Home task 3.3\n";
+	double s = 0;
+	for (int i = 0; i <= 1000; i++) {
+		s += i;
+	}
+	cout << s / 1000;*/
+	/*cout << "Home task 3.4\n";
+	long  p = 1;
+	int a;
+	cin >> a;
+	for (int i = a; i <= 20; i++)
+		p *= i;
+	cout << p << endl;
+	return 0;*/
+	/*cout << "Home task 3.5\n";
+	cout << "Введите число:";
+	int k;
+	cin >> k;
+	for (int i = 1; i != 10; ++i)
+	cout << k << " x " << i << " = " << k * i << '\n';*/
+	/*cout << "Home task 3.6\n";
+	cout << "Введите число: ";
+	int n;
+	cin >> n;
+	for (int i = 0; i <= n; ++i)
+		cout << i << ' ';
+	cout << endl;*/
+	/*cout << "Home task 3.7\n";
+	int a, b;
+
+	cout << "Введите 1 границу диапазона:";
+	cin >> a;
+	cout << "Введите 2 границу диапазона:";
+	cin >> b;
+
+	if (a > b)
+	{
+		int x = a;
+		a = b;
+		b = x;
+	}
+
+
+	cout << "\nЧетные числа:";
+	for (int i = a; i < b; i++)
+		if (i % 2 == 0)
+			cout << i << " ";
+
+	cout << "\nНечетные числа:";
+	for (int i = a; i < b; i++)
+		if (i % 2 != 0)
+			cout << i << " ";
+
+	cout << "\nДелятся на 7: ";
+	for (int i = a; i < b; i++)
+		if (i % 7 == 0)
+			cout << i << " ";*/
+	/*cout << "Home task 3.8\n";
+	int a, b;
+	cout << "Введите 1 границу диапазона:";
+	cin >> a;
+	cout << "Введите 2 границу диапазона:";
+	cin >> b;
+	int res = 0;
+	for (int i = a; i <= b; i++)
+	{
+		res += i;
+	}
+	cout << "Сумма чисел в заданном диапазоне равна " << res << endl;*/
+	/*cout << "Home task 3.9\n";
+	int a = 0;
+	int sum = 0;
+	for (;;)
+	{
+		cout << "Введите число:";
+		cin >> a;
+		if (a == 0)
+			break;
+		sum += a;
+	}
+	cout << "\nСумма: " << sum << "\n\n";*/
+	
+}
+
+
+
