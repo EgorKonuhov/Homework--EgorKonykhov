1f336962e1a301225f671a11cc95e72129913742
EgorKonuhov <115095231+EgorKonuhov@users.noreply.github.com>
2022-11-01 22:09:08 +0300

    Update дз4.cpp

--- дз4.cpp
+++ дз4.cpp
@@ -6,7 +6,110 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
     cout << "Домашнее задание 4.1\n";
-   
+   int s;
+    int R;
+    do
+    {
+        cout<< "Введите размер стороны квадрата: ";
+        cin>>R;
+
+        if (R <= 0)
+        {
+            cout<< "Сторона квадрата должна быть больше 0."<< endl;
+            return 0;
+        }
+
+        cout<< R"( 
+_______ _______ _______ _______ _______
+|*****| |*    | |*****| |     | |*****|
+|**** | |**   | | *** | |     | | *** |
+|***  | |***  | |  *  | |  *  | |  *  |
+|**   | |**** | |     | | *** | | *** |
+|*    | |*****| |     | |*****| |*****|
+   1       2       3       4       5
+_______ _______ _______ _______ _______
+|*   *| |*    | | *   | |*****| |    *|
+|** **| |**   | | **  | |**** | |   **|
+|*****| |***  | | *** | |***  | |  ***|
+|** **| |**   | | **  | |**   | | ****|
+|*   *| |*    | |    *| |*    | |*****|
+
+   6       7       8       9       10
+)" << endl;
+
+        cout << "Выбирите вариант для построения (1-10). 0 для выхода.";
+        cin >> s;
+
+        for (int i = 1; i <= R; i++)
+        {
+            for (int j = 1; j <= R; j++)
+            {
+                switch (s)
+                {
+                case 0:
+                    return 0;
+                    break;
+                case 1: if (j >= i)
+                    cout << "*"; 
+                      else
+                    cout << " ";
+                    break;
+                case 2: if (j <= i)
+                    cout << "*";
+                      else
+                    cout << " ";
+                    break;
+                case 3: if (j >= i && i + j <= R + 1)
+                    cout << "*"; 
+                      else 
+                    cout << " ";
+                    break;
+                case 4: if (j <= i && i + j >= R + 1)
+                    cout << "*";
+                      else
+                    cout << " ";
+                    break;
+                case 5: if (j >= i && i + j <= R + 1 || j <= i && i + j >= R + 1)
+                    cout << "*";
+                      else
+                    cout << " ";
+                    break;
+                case 6: if (j <= i && i + j <= R + 1 || j >= i && i + j >= R + 1)
+                    cout << "*";
+                      else 
+                    cout << " ";
+                    break;
+                case 7: if (j <= i && i + j <= R + 1)
+                    cout << "*";
+                      else
+                    cout << " ";
+                    break;
+                case 8: if (j >= i && i + j >= R + 1)
+                    cout << "*";
+                      else
+                    cout << " ";
+                    break;
+                case 9: if (j + i > R + 1)
+                    cout << " ";
+                      else
+                    cout << "*";
+                    break;
+                case 10: if (j + i < R + 1)
+                    cout << " ";
+                       else
+                    cout << "*";
+                    break;
+                }
+            }
+            cout << endl;
+        }
+
+
+    } while (s);
+
+    return 0;
+}
+
     /*cout << "Домашнее задание номер 4.2\n";
     int k = 0;
     int i1, i2, i3;
