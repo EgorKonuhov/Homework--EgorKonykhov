6129726e5d0b540539325614040f7f512eb8b418
EgorKonuhov <115095231+EgorKonuhov@users.noreply.github.com>
2022-11-06 19:21:25 +0300

    Add files via upload

---
+++ ConsoleApplication27.cpp
@@ -0,0 +1,68 @@
+﻿// ConsoleApplication27.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+ 
+//int NUMBER(int number, int Num)
+//{
+//    int res = 1;
+//    for (int i = 0; i < Num-1; i++)
+//        return res *= number;
+//}
+//int sum(int a, int b) { 
+//    int s = 0;
+//    for (int i = a + 1; i < b; i++)
+//        s += i;
+//    return(s);
+//}
+//int dig(int a, int b)
+//{
+//    for (int i = a; i < b; i++)
+//    {
+//        int result = 0;
+//        for (int j = 1; j < i; j++)
+//        {
+//            if ((i % j) == 0)
+//            {
+//                result += j;
+//            }
+//        }
+//        if (result == i && result)
+//        {
+//            return result;
+//        }
+//    }
+//}
+//int isHappy(unsigned int num)
+//{
+//    if (num < 100000 || num > 999999)
+//        return -1;
+//    return ((num / 100000) + ((num / 10000) % 10) + ((num / 1000) % 10)) == (((num / 100) % 10) + ((num / 10) % 10) + (num % 10));
+//}
+int main()
+{ 
+    setlocale(0, "Russian");
+    /*cout << "Home task 5.1\n";
+    int number, Num;
+    cout << "Введите number: ";
+    cin >> number;
+    cout << "Введите Num: ";
+    cin >> Num;
+    cout << "Результат:" << NUMBER(number, Num) << endl;*/
+    /*cout << "Home task 5.2\n";
+    int a, b;
+    cin >> a >> b;
+    cout << (sum(a, b));*/
+    /*cout << "Home task 5.3\n";
+    int res = dig(0, 9000);
+    cout << res << " \n";*/
+    /*cout << "Home task 5.5";
+    int res = isHappy(267456);
+    if (res == 1)
+        cout << "Счастливое" << endl;
+    else if (res == 0)
+        cout << "НЕ счастливое" << endl;*/
+    cout << "Home task 5.4";
+
+}
